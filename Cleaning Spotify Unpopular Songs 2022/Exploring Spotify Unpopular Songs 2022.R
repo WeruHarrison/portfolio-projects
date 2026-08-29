0727af51adfce5967400153a413c5f0764c127d9
Weru Harrison <weruharim@gmail.com>
2026-08-29 14:14:58 +0300

    identified skewness from summary statistics and confirmed using histogram)

--- Cleaning Spotify Unpopular Songs 2022/Exploring Spotify Unpopular Songs 2022.R
+++ Cleaning Spotify Unpopular Songs 2022/Exploring Spotify Unpopular Songs 2022.R
@@ -11,13 +11,13 @@ sum(duplicated(df$track_id))
 
 # Data cleaning: the 'genre' variable is the cause of the 1700 duplicates shown by the 'tracl_id' variable. So, I subset the original df without the 'genre' variable.
 df_nogenre <- df %>%
+  select(!genre) %>%
   distinct(track_id, .keep_all = TRUE)
 
 # Understanding the target variable: popularity
-
 range(df_nogenre$popularity)
 
-## frequency ditribution
+## frequency distribution
 df_nogenre %>%
   count(popularity) %>%
   mutate(
@@ -27,8 +27,39 @@ df_nogenre %>%
 ## visualising the frequency distribution
 ggplot(data = df_nogenre) +
   geom_bar(mapping = aes(x=popularity))+
+  theme_bw()+
   labs(y=NULL,
        x = "Popularity Score",
        y = "Number of Songs",
        title = "Frequency Distribution of Popularity Scores")
+
+# some notable variables to study:----
+# 1. speechiness: Possibly right skewed because median<mean
+# 2. acousticness: Possibly right skewed because median<mean
+# 3. liveness: Possibly right skewed because median<mean
+# 4. tempo: has a minimum tempo of 0 BPM, which is unusual for music
+# 5. duration_min: possible outliers as the max duration is over 56m compared to the median of 3.26m.
+
+## Confirming the skewness of speechiness, acouticness, and liveness
+hist_acousticness <- hist(df_nogenre$acousticness, 
+                          main = "Histogram of Acousticness", 
+                          xlab =  "Acousticness")
+hist_speechiness <- hist(df_nogenre$speechiness, 
+                         main = "Histogram of speechiness", 
+                         xlab =  "speechiness")
+hist_liveness <- hist(df_nogenre$liveness, 
+                      main = "Histogram of liveness", 
+                      xlab =  "liveness")
+
+
+
+
+
+
+
+
+
+
+
+
 
